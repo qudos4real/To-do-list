e69bd973617efcda66a0e344819d5472072690ce
Qudos4real <qudos4real@yahoo.com>
2023-03-11 21:30:52 +0100

    changed function names again

--- index.js
+++ index.js
@@ -7,22 +7,22 @@ function onSubmit(e) {
   e.preventDefault();
   const input = document.getElementById("inputValue").value;
   lists.push(input);
-  addNewTask();
+  addNewTaskToList();
   document.getElementById("inputValue").value = "";
 }
 
-function addNewTask() {
+function addNewTaskToList() {
   const output = document.getElementById("output");
   output.innerHTML = "";
   for (let index in lists) {
     let list = lists[index];
-    output.innerHTML += `<div id='output' onclick='deleteTask(${index})' class='output'> ${list}</div>`;
+    output.innerHTML += `<div id='output' onclick='deleteTaskFromList(${index})' class='output'> ${list}</div>`;
   }
 }
 
-function deleteTask(index) {
+function deleteTaskFromList(index) {
   lists = lists.filter((word, i) => i != index);
-  addNewTask();
+  addNewTaskToList();
 }
 
 
